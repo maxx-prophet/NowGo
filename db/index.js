cc996478de88f87488de5cdaa1285c0dbadb8c79
maxx-prophet <donnieraybolen@gmail.com>
2026-05-26 22:16:07 -0400

    Fix DB SSL: always use rejectUnauthorized:false for Railway Postgres

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30_000,
   connectionTimeoutMillis: 5_000,
@@ -17,3 +17,4 @@ pool.on("error", (err) => {
 });
 
 export default pool;
+
