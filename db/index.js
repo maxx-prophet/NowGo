f019bf4f90ef6453a06a23966738c0232348cec4
maxx-prophet <donnieraybolen@gmail.com>
2026-05-26 22:21:49 -0400

    Fix DB SSL: disable SSL entirely for Railway internal Postgres

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: false,
   max: 10,
   idleTimeoutMillis: 30_000,
   connectionTimeoutMillis: 5_000,
